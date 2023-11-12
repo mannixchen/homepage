4e625606d5b5ebc9829712f8d4411224ccbe3216
mannixchan <51387089+mannixchan@users.noreply.github.com>
2023-11-12 18:57:24 +0800

    good

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import {BILIBILISPACE, YOUTUBESPACE} from './const.js'
+</script>
+
+<template>
+  <div>
+    <a :href="BILIBILISPACE" target="_blank">
+      BILIBILI
+    </a>
+    <a :href="YOUTUBESPACE" target="_blank">
+      YOUTUBE
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+.logo {
+  height: 1em;
+  padding: .5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.bilibili:hover {
+  filter: drop-shadow(0 0 2em #F9F9F9);
+}
+.logo.youtube:hover {
+  filter: drop-shadow(0 0 2em #F9F9F9);
+}
+</style>
---
+++ src/const.js
@@ -0,0 +1,4 @@
+export const BILIBILISPACE = "https://space.bilibili.com/68406680",
+  YOUTUBESPACE = "https://www.youtube.com/@mannixchen"
+
+
